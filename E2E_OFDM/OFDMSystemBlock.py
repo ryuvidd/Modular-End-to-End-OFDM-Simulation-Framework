13f7c283f0b52afe300635488c8c69ecec56ecc2
ryuvidd <nattakrit29436@gmail.com>
2026-04-24 15:08:01 +0800

    separate config and metadata

--- E2E_OFDM/OFDMSystemBlock.py
+++ E2E_OFDM/OFDMSystemBlock.py
@@ -17,11 +17,14 @@ class OFDMconfig:
     LengthCP: int
     SNR: np.ndarray
     PilotTypes: PILOT_TYPES
+    Estimator: CHANNEL_ESTIMATOR
+    Interpolator: CHANNEL_INTERPOLATOR
+
+    # Channel Configuration 
     ChannelModel: CHANNEL_MODEL
     NumTap: int
     RegenChannel: int
-    Estimator: CHANNEL_ESTIMATOR
-    Interpolator: CHANNEL_INTERPOLATOR
+
 
 class Transmitter():
     def __init__(self, config) -> None:
@@ -35,24 +38,28 @@ class Transmitter():
         TransmittedSymbols = self.Modulator.modulate(Blocks)
         OFDMSymbols = self.OFDMModulator.modulate(TransmittedSymbols)
         self.SpectrumViewer.PlotPSD(OFDMSymbols)
-        OFDMconfig.PilotIndices = self.BlocksGenerator.PilotIndices
-        OFDMconfig.TransmittedSymbols = TransmittedSymbols
-        return OFDMSymbols, DataBits, OFDMconfig
+
+        metadata = {}
+        metadata["PilotIndices"] = self.BlocksGenerator.PilotIndices
+        metadata["TransmittedSymbols"] = TransmittedSymbols
+        metadata["DataBits"] = DataBits
+        return OFDMSymbols, OFDMconfig, metadata
     
 class ChannelBlock():
     def __init__(self, config) -> None:
         self.ChannelGenerator = SelectChannelModel(config)
         self.NoiseMixer = NoiseMixer()
 
-    def process(self, OFDMSymbols: np.ndarray, OFDMconfig) -> tuple:
+    def process(self, OFDMSymbols: np.ndarray, OFDMconfig, metadata) -> tuple:
         ChannelOutput = self.ChannelGenerator.process(OFDMSymbols, OFDMconfig.NumSubCarrier)
         ReceivedSignals = []
         for snr in OFDMconfig.SNR:
             ReceivedSignals.append(self.NoiseMixer.process(ChannelOutput, snr))
         ReceivedSignals = np.stack(ReceivedSignals, axis=0)
-        OFDMconfig.VarNoises = np.stack(self.NoiseMixer.VarNoises, axis=0)
-        OFDMconfig.ChannelsFreqDomain = self.ChannelGenerator.Channels_feq
-        return ReceivedSignals, OFDMconfig
+
+        metadata["VarNoises"] = np.stack(self.NoiseMixer.VarNoises, axis=0)
+        metadata["ChannelsFreqDomain"] = self.ChannelGenerator.Channels_feq
+        return ReceivedSignals, OFDMconfig, metadata
     
 class Receiver():
     def __init__(self, config) -> None:
@@ -63,21 +70,21 @@ class Receiver():
         self.TotalNumBlock = config.TotalNumBlock
         self.Equalizer = ZeroForcing()
     
-    def process(self, ReceivedSignals: np.ndarray, OFDMconfig) -> tuple:
-        PilotIndices = OFDMconfig.PilotIndices
+    def process(self, ReceivedSignals: np.ndarray, OFDMconfig, metadata) -> tuple:
+        PilotIndices = metadata["PilotIndices"]
         EstimatedChannels = []
         EstimatedBits = []
         for snr_ind in range(len(OFDMconfig.SNR)):
             ReceivedSymbols = self.OFDMDemodulator.demodulate(ReceivedSignals[snr_ind])
             # Channel Estimation #
             ReceivedPilots = np.where(PilotIndices, ReceivedSymbols, 0)
-            TransmittedPilots = np.where(PilotIndices, OFDMconfig.TransmittedSymbols, 0)
-            metadata = {
-                "VarNoise": OFDMconfig.VarNoises[snr_ind],
-                "ChannelsFreqDomain": OFDMconfig.ChannelsFreqDomain,
+            TransmittedPilots = np.where(PilotIndices, metadata["TransmittedSymbols"], 0)
+            EstimationMetadata = {
+                "VarNoise": metadata["VarNoises"][snr_ind],
+                "ChannelsFreqDomain": metadata["ChannelsFreqDomain"],
                 "LengthCP": OFDMconfig.LengthCP
             }
-            EstimatedChannel = self.ChannelEstimator.process(ReceivedPilots, TransmittedPilots, metadata)
+            EstimatedChannel = self.ChannelEstimator.process(ReceivedPilots, TransmittedPilots, EstimationMetadata)
             EstimatedChannel = self.ChannelInterpolator.process(EstimatedChannel)
             EstimatedChannels.append(EstimatedChannel)
             # Equalization #
@@ -99,16 +106,16 @@ class OFDMSystem():
         self.Evaluater = TotalEvaluators(config.SNR)
 
     def run(self):
-        OFDMSymbols, DataBits, config = self.Transmitter.process(self.OFDMconfig)
+        OFDMSymbols, config, metadata = self.Transmitter.process(self.OFDMconfig)
         logging.info("Transmitted signals")
-        ReceivedSignals, config = self.ChannelBlock.process(OFDMSymbols, config)
+        ReceivedSignals, config, metadata = self.ChannelBlock.process(OFDMSymbols, config, metadata)
         logging.info("Received signals and start recovering signal")
-        EstimatedBits, EstimatedChannels = self.Receiver.process(ReceivedSignals, config)
+        EstimatedBits, EstimatedChannels = self.Receiver.process(ReceivedSignals, config, metadata)
         logging.info("Data recovered")
 
         # Duplicate for each SNR
-        DataBits = np.tile(DataBits, (len(self.OFDMconfig.SNR),1,1))
-        ChannelFreqDomain = np.tile(config.ChannelsFreqDomain, (len(self.OFDMconfig.SNR),1,1))
+        DataBits = np.tile(metadata["DataBits"], (len(self.OFDMconfig.SNR),1,1))
+        ChannelFreqDomain = np.tile(metadata["ChannelsFreqDomain"], (len(self.OFDMconfig.SNR),1,1))
 
         EstimatedData = ExperimentData(
             Bits=EstimatedBits, 
